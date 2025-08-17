f4e6856737f8d7b27002f91688badd76b38c5d98
CRASHs4aint <samratkumarsahu228@gmail.com>
2025-08-18 00:13:02 +0530

    add hero animation

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+
+    .to(".boundingelem", {
+      y: 0,
+      duration: 2,
+      ease: Expo.easeInOut,
+      delay:-1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay:-1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+  });
+}
+circleMouseFollower();
+firstPageAnim();
